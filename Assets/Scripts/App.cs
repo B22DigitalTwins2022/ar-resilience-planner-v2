f9670f09401a50a4d8d00b18962ad8132700afc7
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2022-06-03 16:01:36 +0200

    UI Work

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class App : MonoBehaviour
+namespace ShapeReality.ARResiliencePlanner
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>The entry point of the entire app. Any initialisation is done here</summary>
+    public sealed class App : MonoBehaviour
     {
-        
-    }
+        private const string SCENENAME_ENVIRONMENT = "Environment";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void Awake()
+        {
+            //Application.targetFrameRate = 60;
+
+            InitializeScene(SCENENAME_ENVIRONMENT);
+        }
+
+        private void InitializeScene(string sceneName)
+        {
+#if UNITY_EDITOR
+            if (SceneManager.GetSceneByName(sceneName) != null) { return; };
+#endif
+            AsyncOperation op = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            op.completed += (AsyncOperation result) =>
+            {
+                print("SceneEditor initialized");
+            };
+        }
     }
 }
