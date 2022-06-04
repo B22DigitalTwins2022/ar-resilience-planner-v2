e7db487e66561baa46c0c9a47a17f4735ff40b60
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2022-06-04 15:37:25 +0200

    Start on getting input from controllers

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Menu : MonoBehaviour
+namespace ShapeReality
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Menu : MonoBehaviour, XRIDefaultInputActions.IXRILeftHandInteractionActions, XRIDefaultInputActions.IXRIRightHandInteractionActions
     {
-        
-    }
+        private XRIDefaultInputActions m_InputActions;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void OnEnable()
+        {
+            if (m_InputActions == null)
+            {
+                m_InputActions = new XRIDefaultInputActions();
+                m_InputActions.XRILeftHandInteraction.SetCallbacks(this);
+                m_InputActions.XRIRightHandInteraction.SetCallbacks(this);
+            }
+        }
+
+        void XRIDefaultInputActions.IXRILeftHandInteractionActions.OnActivate(InputAction.CallbackContext context) { } // Activate is bound to the trigger
+        void XRIDefaultInputActions.IXRILeftHandInteractionActions.OnActivateValue(InputAction.CallbackContext context) { }
+        void XRIDefaultInputActions.IXRILeftHandInteractionActions.OnRotateAnchor(InputAction.CallbackContext context) { }
+        void XRIDefaultInputActions.IXRILeftHandInteractionActions.OnSelect(InputAction.CallbackContext context) { } // Select is bound to the grip button
+        void XRIDefaultInputActions.IXRILeftHandInteractionActions.OnSelectValue(InputAction.CallbackContext context) { }
+        void XRIDefaultInputActions.IXRILeftHandInteractionActions.OnTranslateAnchor(InputAction.CallbackContext context) { }
+        void XRIDefaultInputActions.IXRILeftHandInteractionActions.OnUIPress(InputAction.CallbackContext context) { } // Bound to the trigger
+        void XRIDefaultInputActions.IXRILeftHandInteractionActions.OnUIPressValue(InputAction.CallbackContext context) { }
+
+        void XRIDefaultInputActions.IXRIRightHandInteractionActions.OnActivate(InputAction.CallbackContext context) { } // Activate is bound to the trigger
+        void XRIDefaultInputActions.IXRIRightHandInteractionActions.OnActivateValue(InputAction.CallbackContext context) { }
+        void XRIDefaultInputActions.IXRIRightHandInteractionActions.OnRotateAnchor(InputAction.CallbackContext context) { }
+        void XRIDefaultInputActions.IXRIRightHandInteractionActions.OnSelect(InputAction.CallbackContext context) { } // Select is bound to the grip button
+        void XRIDefaultInputActions.IXRIRightHandInteractionActions.OnSelectValue(InputAction.CallbackContext context) { }
+        void XRIDefaultInputActions.IXRIRightHandInteractionActions.OnTranslateAnchor(InputAction.CallbackContext context) { }
+        void XRIDefaultInputActions.IXRIRightHandInteractionActions.OnUIPress(InputAction.CallbackContext context) { } // Bound to the trigger
+        void XRIDefaultInputActions.IXRIRightHandInteractionActions.OnUIPressValue(InputAction.CallbackContext context) { }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
