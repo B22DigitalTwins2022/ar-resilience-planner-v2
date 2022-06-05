e7c93b3b81a4418f543cd75c9d636bf6a3dfe41a
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2022-06-05 16:27:45 +0200

    Fixed raycasting and moving the DraggableObject through the scene

--- Assets/Scripts/DraggableObject.cs
+++ Assets/Scripts/DraggableObject.cs
@@ -8,35 +8,102 @@ namespace ShapeReality
 {
     public class DraggableObject : XRBaseInteractable
     {
+        private const string DEFAULT_LAYERMASK_STRING = "Default";
+        private const string DRAGGING_OBJECT_LAYERMASK_STRING = "DraggingObject";
+
+        private LayerMask m_DefaultLayerMask;
+        private LayerMask m_DefaultLayerMaskIndex;
+        private LayerMask m_DraggingObjectLayerMask;
+        private LayerMask m_DraggingObjectLayerMaskIndex;
+
+        //private IXRInteractor m_Interactor;
+        private Transform m_RayOriginTransform;
+        private bool m_IsDragging;
+
+        private MeshRenderer m_Renderer;
+
         public void Start()
         {
-            
+            m_DefaultLayerMask = LayerMask.GetMask(DEFAULT_LAYERMASK_STRING);
+            m_DefaultLayerMaskIndex = LayerMask.NameToLayer(DEFAULT_LAYERMASK_STRING);
+            m_DraggingObjectLayerMask = LayerMask.GetMask(DRAGGING_OBJECT_LAYERMASK_STRING);
+            m_DraggingObjectLayerMaskIndex = LayerMask.NameToLayer(DRAGGING_OBJECT_LAYERMASK_STRING);
+            m_Renderer = GetComponent<MeshRenderer>();
+        }
+
+        public void Update()
+        {
+            if (m_IsDragging)
+            {
+                UpdateObjectTransform();
+            }
         }
 
         protected override void OnHoverEntered(HoverEnterEventArgs args)
         {
             base.OnHoverEntered(args);
-            DebugText.Log("onHoverEntered");
         }
 
         protected override void OnHoverExited(HoverExitEventArgs args)
         {
             base.OnHoverExited(args);
-            DebugText.Log("onHoverExited");
         }
 
         protected override void OnSelectEntered(SelectEnterEventArgs args)
         {
             base.OnSelectEntered(args);
-            DebugText.Log("onSelectEntered");
+            
+
+            m_Renderer.material.color = Color.blue;
+
+            m_RayOriginTransform = args.interactorObject.GetAttachTransform(this);
+            gameObject.layer = m_DraggingObjectLayerMaskIndex;
+            //DebugText.Log(string.Format("{0}", gameObject.layer));
+            m_IsDragging = true;
         }
 
         protected override void OnSelectExited(SelectExitEventArgs args)
         {
             base.OnSelectExited(args);
-            DebugText.Log("onSelectExited");
+            //DebugText.Log(string.Format("{0}", gameObject.layer));
+
+
+            m_IsDragging = false;
+
+            m_Renderer.material.color = Color.white;
+
+            gameObject.layer = m_DefaultLayerMaskIndex;
         }
 
-        //protected override void OnHover
+        private void UpdateObjectTransform()
+        {
+            if (m_RayOriginTransform == null) { return; }
+            // Update the transform based on the raycast etc
+
+            // Perform a raycast into the scene
+            RaycastHit hit;
+
+            if (Physics.Raycast(InteractorRay, out hit, Mathf.Infinity, m_DefaultLayerMask))
+            {
+                // A hit has been made, see if it is a dropzone, otherwise just move it to the place
+                DropZone dropZone = hit.collider.GetComponent<DropZone>();
+
+                // Interpolate these positions (look at XR Interaction Toolkit for reference
+                if (dropZone != null)
+                {
+                    // Align it to the dropzone
+                    transform.position = dropZone.transform.position;
+                } else
+                {
+                    // Just move it to the aimed at location
+                    transform.position = hit.point;
+                }
+            }
+        }
+
+        private Ray InteractorRay
+        {
+            get => new(m_RayOriginTransform.position, m_RayOriginTransform.forward);
+        }
     }
 }
