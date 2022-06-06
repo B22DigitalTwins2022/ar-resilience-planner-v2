a3c34bf241caea21d74e6cbcfa350586c1878c42
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2022-06-06 14:12:08 +0200

    commit

--- Assets/Scripts/Solutions/SolutionUIObject.cs
+++ Assets/Scripts/Solutions/SolutionUIObject.cs
@@ -14,14 +14,56 @@ namespace ShapeReality
         public Solution solution;
         public SolutionsPanel solutionsPanel;
 
+        public RectTransform hoverOffset;
+
+        public Renderer hoverVisualRenderer;
+
+        private float m_TargetZPosition = 0.0f;
+        private float m_CurrentZPosition = 0.0f;
+
+        private float m_TargetHoverAlpha = 0.0f;
+        private float m_CurrentHoverAlpha = 0.0f;
+        private const float HOVER_ALPHA = 0.4f;
+        private Color hoverColor;
+
+        private float m_EndpointSmoothingTime = 0.02f;
+
+        private const float HOVER_UI_HEIGHT = -0.3f; 
+
+
+        public void Start()
+        {
+            hoverColor = hoverVisualRenderer.material.color;
+            hoverVisualRenderer.material.color = SetAlpha(hoverColor, 0);
+        }
+
+        public void Update()
+        {
+            if (Interpolate(m_TargetZPosition, m_CurrentZPosition, out float z))
+            {
+                hoverOffset.localPosition = SetZ(hoverOffset.localPosition, z);
+            }
+
+            if (Interpolate(m_TargetHoverAlpha, m_CurrentHoverAlpha, out float alpha))
+            {
+                hoverVisualRenderer.material.color = SetAlpha(hoverColor, alpha);
+            }
+        }
+
+
+        
         protected override void OnHoverEntered(HoverEnterEventArgs args)
         {
             base.OnHoverEntered(args);
+            m_TargetZPosition = HOVER_UI_HEIGHT;
+            m_TargetHoverAlpha = HOVER_ALPHA;
         }
 
         protected override void OnHoverExited(HoverExitEventArgs args)
         {
             base.OnHoverExited(args);
+            m_TargetZPosition = 0.0f;
+            m_TargetHoverAlpha = 0.0f;
         }
 
         protected override void OnSelectEntered(SelectEnterEventArgs args)
@@ -32,6 +74,30 @@ namespace ShapeReality
         protected override void OnSelectExited(SelectExitEventArgs args)
         {
             base.OnSelectExited(args);
+        }
+
+        // Methods for interpolating smoothly
+
+        private bool Interpolate(float current, float target, out float output)
+        {
+            if (Mathf.Abs(target - current) > Mathf.Epsilon)
+            {
+                var velocity = 0.0f;
+                output = Mathf.SmoothDamp(current, target, ref velocity, m_EndpointSmoothingTime);
+                return true;
+            }
+            output = 0;
+            return false;
+        }
+
+        private Vector3 SetZ(Vector3 vector, float zComponent)
+        {
+            return new Vector3(vector.x, vector.y, zComponent);
+        }
+
+        private Color SetAlpha(Color color, float alpha)
+        {
+            return new Color(color.r, color.g, color.b, alpha);
         }
     }
 }
